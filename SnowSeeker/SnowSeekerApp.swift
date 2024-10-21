6dead9e5ecb17728a9c5a69fe6cff15bbb7fd425
Víctor Ávila <victoraavila2000@gmail.com>
2024-10-21 00:01:24 -0300

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Víctor Ávila on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
